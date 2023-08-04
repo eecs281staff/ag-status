2a7b6b21ca069e9d0ad272ea4561ea01500696b4
devStorm <59678453+developStorm@users.noreply.github.com>
2023-08-05 04:25:52 +0800

    feat: header status card

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ function NavItem({
   const activeItemClass =
     " bg-black/80 dark:bg-white/70 dark:text-mblue py-0.5 px-2 rounded-md text-maize text-bold";
   return (
-    <li className={"inline font-bold" + (active ? activeItemClass : "")}>
+    <li className={"inline-block" + (active ? activeItemClass : "")}>
       <a href={link} rel="noopener" target="_blank">
         {children}
       </a>
@@ -20,36 +20,49 @@ function NavItem({
 
 export default function Header() {
   return (
-    <header className="bg-maize dark:bg-mblue md:px-14 py-8 px-8">
-      <div className="flex flex-col gap-5 md:flex-row justify-between items-center mx-auto md:w-5/6 xl:w-4/6 2xl:w-3/6">
-        <div>
-          <span className="flex items-center -mt-2 ml-0.5 font-extralight">
-            EECS 281{" "}
-            <svg
-              viewBox="0 0 22 22"
-              aria-hidden
-              role="img"
-              className="w-5 h-5 inline ml-0.5 -mt-0.5 fill-mblue dark:fill-maize"
-            >
-              <g>
-                <path d="M20.396 11a3.487 3.487 0 0 0-2.008-3.062 3.474 3.474 0 0 0-.742-3.584 3.474 3.474 0 0 0-3.584-.742A3.468 3.468 0 0 0 11 1.604a3.463 3.463 0 0 0-3.053 2.008 3.472 3.472 0 0 0-1.902-.14c-.635.13-1.22.436-1.69.882a3.461 3.461 0 0 0-.734 3.584A3.49 3.49 0 0 0 1.604 11a3.496 3.496 0 0 0 2.017 3.062 3.471 3.471 0 0 0 .733 3.584 3.49 3.49 0 0 0 3.584.742A3.487 3.487 0 0 0 11 20.396a3.476 3.476 0 0 0 3.062-2.007 3.335 3.335 0 0 0 4.326-4.327A3.487 3.487 0 0 0 20.396 11zM9.662 14.85l-3.429-3.428 1.293-1.302 2.072 2.072 4.4-4.794 1.347 1.246z" />
-              </g>
-            </svg>
-          </span>
-          <h1 className="text-3xl md:text-4xl align-middle font-extrabold">
-            Autograder{" "}
-            <span className="text-black/60 dark:text-white/80">Status</span>
-          </h1>
+    <header className="bg-maize dark:bg-mblue pt-10 px-8 md:px-14">
+      <div className="mx-auto md:w-5/6 xl:w-4/6 2xl:w-3/6 max-w-[1000px]">
+        <div className="flex flex-col gap-5 md:flex-row justify-between items-center">
+          <div>
+            <span className="flex items-center -mt-2 ml-0.5 font-extralight">
+              EECS 281{" "}
+              <svg
+                viewBox="0 0 22 22"
+                aria-hidden
+                role="img"
+                className="w-5 h-5 inline ml-0.5 -mt-0.5 fill-mblue dark:fill-maize"
+              >
+                <g>
+                  <path d="M20.396 11a3.487 3.487 0 0 0-2.008-3.062 3.474 3.474 0 0 0-.742-3.584 3.474 3.474 0 0 0-3.584-.742A3.468 3.468 0 0 0 11 1.604a3.463 3.463 0 0 0-3.053 2.008 3.472 3.472 0 0 0-1.902-.14c-.635.13-1.22.436-1.69.882a3.461 3.461 0 0 0-.734 3.584A3.49 3.49 0 0 0 1.604 11a3.496 3.496 0 0 0 2.017 3.062 3.471 3.471 0 0 0 .733 3.584 3.49 3.49 0 0 0 3.584.742A3.487 3.487 0 0 0 11 20.396a3.476 3.476 0 0 0 3.062-2.007 3.335 3.335 0 0 0 4.326-4.327A3.487 3.487 0 0 0 20.396 11zM9.662 14.85l-3.429-3.428 1.293-1.302 2.072 2.072 4.4-4.794 1.347 1.246z" />
+                </g>
+              </svg>
+            </span>
+            <h1 className="text-3xl md:text-4xl align-middle font-extrabold">
+              Autograder{" "}
+              <span className="text-black/60 dark:text-white/80">Status</span>
+            </h1>
+          </div>
+          <nav>
+            <ul className="flex gap-3 items-center whitespace-nowrap">
+              <NavItem link="#" active>
+                Current Status
+              </NavItem>
+              <NavItem link="https://piazza.com/">Piazza</NavItem>
+              <NavItem link="https://www.gradescope.com/">Gradescope</NavItem>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul className="flex gap-3 items-center whitespace-nowrap">
-            <NavItem link="#" active>
-              Current Status
-            </NavItem>
-            <NavItem link="https://piazza.com/">Piazza</NavItem>
-            <NavItem link="https://www.gradescope.com/">Gradescope</NavItem>
-          </ul>
-        </nav>
+        <section className="relative top-6 bg-white shadow dark:bg-pumablack dark:ring-2 dark:ring-green-800 rounded-md p-4 flex flex-row items-center gap-4">
+          <span
+            role="img"
+            aria-hidden
+            className="w-8 h-8 border-2 border-green-500 rounded-full bg-cover bg-no-repeat inline-block bg-[url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjgiIGhlaWdodD0iMjgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyLjU5MSAxNi43ODhsOC45Ny04Ljk3IDIuMTIxIDIuMTIxLTEwLjk4NSAxMC45ODUtLjEwNi0uMTA2LS4xMDYuMTA2LTYuNjY3LTYuNjY3IDIuMTIxLTIuMTIxIDQuNjUyIDQuNjUyeiIgZmlsbD0iIzREQzQ3RSIvPjwvc3ZnPg==)]"
+          ></span>
+          <div>
+            <h2 className="text-lg font-bold">Recommend using: Node A</h2>
+            <p className="text-sm text-green-500">All systems operational</p>
+          </div>
+        </section>
       </div>
     </header>
   );
