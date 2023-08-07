46470eec576d266031381593a8664ebc60577fbc
devStorm <59678453+developStorm@users.noreply.github.com>
2023-08-07 08:52:46 +0800

    refactor: server list config file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,24 +11,9 @@ import {
   ServerStatusSkeleton,
 } from "@/components/status-card";
 
+import servers from "@/data/servers.json";
+
 export default function Home() {
-  const [servers, setServers] = useState<Server[]>([
-    {
-      url: "https://eecs281a.eecs.umich.edu/",
-      name: "Server A",
-      capacity: 6,
-    },
-    {
-      url: "https://eecs281b.eecs.umich.edu/",
-      name: "Server B",
-      capacity: 6,
-    },
-    {
-      url: "https://eecs281c.eecs.umich.edu/",
-      name: "Server C",
-      capacity: 6,
-    },
-  ]);
   const [status, setStatus] = useState<ServerStatus>({});
   const [inFlight, setInFlight] = useState<Record<string, boolean>>({});
 
@@ -52,14 +37,14 @@ export default function Home() {
         setInFlight((prev) => ({ ...prev, [server.url]: false }));
       });
     });
-  }, [servers, status, inFlight]);
+  }, [status, inFlight]);
 
   return (
     <div className="flex min-h-screen flex-col">
       <Header servers={servers} status={status} setStatus={setStatus} />
       <Wrapper>
         <h2 className="mb-5 text-center text-2xl">Autograder Servers</h2>
-        <div className="flex flex-col divide-y overflow-hidden rounded-md border dark:divide-slate-600/60 dark:border-slate-600/60 dark:bg-pumablack">
+        <div className="flex w-full min-w-full flex-col divide-y overflow-hidden rounded-md border dark:divide-slate-600/60 dark:border-slate-600/60 dark:bg-pumablack">
           {servers.map((server) =>
             status[server.url] ? (
               <ServerStatusCard
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <Wrapper className="mt-auto">
       <footer className="p-8 text-center text-sm md:text-base">
-        &copy; University of Michigan, {new Date().getFullYear()}
+        <p>&copy; University of Michigan, {new Date().getFullYear()}</p>
+        <p>Made with ❤️ by 281 staff</p>
       </footer>
     </Wrapper>
   );
