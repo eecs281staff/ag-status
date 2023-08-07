a7a31d4f78b2d1040fbab5d4becb918d5797ca9c
devStorm <59678453+developStorm@users.noreply.github.com>
2023-08-08 06:45:07 +0800

    patch: footer padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,24 @@ import ThemeSwitch from "@/components/theme-switch";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto border-t p-4 text-sm text-zinc-500 dark:border-slate-600/60 md:p-6 md:text-base">
-      <Wrapper>
-        <div className="flex w-full flex-row items-center justify-between">
-          <p>
-            &copy;{" "}
-            <a
-              href="https://cse.engin.umich.edu/"
-              className="transition hover:text-maize dark:hover:text-maize/80"
-            >
-              University of Michigan
-            </a>
-          </p>
-          <ThemeSwitch />
-        </div>
-      </Wrapper>
-    </footer>
+    <>
+      <div className="h-16"></div>
+      <footer className="mt-auto border-t p-4 text-sm text-zinc-500 dark:border-slate-600/60 md:p-6 md:text-base">
+        <Wrapper>
+          <div className="flex w-full flex-row items-center justify-between">
+            <p>
+              &copy;{" "}
+              <a
+                href="https://cse.engin.umich.edu/"
+                className="transition hover:text-maize dark:hover:text-maize/80"
+              >
+                University of Michigan
+              </a>
+            </p>
+            <ThemeSwitch />
+          </div>
+        </Wrapper>
+      </footer>
+    </>
   );
 }
